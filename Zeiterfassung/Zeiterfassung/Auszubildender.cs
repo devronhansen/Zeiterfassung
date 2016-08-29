f27137c1875229e2142322db7833e5612f026e7c
Ron Hansen <ron.hansen112@gmail.com>
2016-08-29 15:34:49 +0200

    Test

---
+++ Zeiterfassung/Zeiterfassung/Auszubildender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeiterfassung
+{
+    class Auszubildender
+    {
+        public string TAG { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+    }
+}
---
+++ Zeiterfassung/Zeiterfassung/connectToDB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace Zeiterfassung
+{
+    class connectToDB
+    {
+        List<Auszubildender> Auszubildende = new List<Auszubildender>();
+
+        public List<Auszubildender> readFromDB()
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(@"server=localhost;userid=root;password=;database=zeiterfassung");
+                conn.Open();
+
+                MySqlCommand command = new MySqlCommand("SELECT * FROM auszubildende", conn);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Auszubildende.Add(new Auszubildender
+                    {
+                        TAG = reader[0].ToString(),
+                        Vorname = reader[1].ToString(),
+                        Nachname = reader[2].ToString()
+                    });
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: {0}", ex);
+            }
+
+            return Auszubildende;
+        }
+
+        public void insertIntoDB()
+        {
+
+        }
+
+        public void updateDB()
+        {
+
+        }
+    }
+}
